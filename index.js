24a41de15319c68f26a75ce94f1182924e94dafa
P Joshi <joshipv@yahoo.com>
2016-08-25 21:07:13 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function countdown(callback){
   window.setTimeout(callback, 2000);
   done();
 }
-function createMultiplier(val1, val2) {
+function multiplier(val1, val2) {
   return function({
     return val1 * val2;
   })
@@ -14,3 +14,5 @@ function doublerWithBind(2) {
 function triplerWithBind(3) {
 
 }
+
+var doubler = createMultiplier()
