e2f4266cacbbb00af5124f01f3467f58f9356e87
P Joshi <joshipv@yahoo.com>
2016-08-25 20:48:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function countdown(callback){
+  window.setTimeout(callback, 2000);
+  done();
+}
+function createMultiplier(val1, val2) {
+  return function({
+    return val1 * val2;
+  })
+}
+
+function doublerWithBind(2) {
+
+}
+function triplerWithBind(3) {
+
+}
